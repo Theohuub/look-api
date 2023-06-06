12ef8a8cf798624526c6ccf1b4758eb61fc58012
theo <theo@email.com>
2023-06-06 20:56:15 -0300

    Inicial Config

---
+++ src/main/java/br/com/ifpe/look/LookApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.look;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/look/LookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.look;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
