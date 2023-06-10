03e00ca9eeba94a89c72dbfd801ea0b783454c1b
QuaiboC <crpas@discente.ifpe.edu.br>
2023-06-10 19:35:36 -0300

    Tabela de produto

---
+++ src/main/java/br/com/ifpe/look/api/produto/ProdutoRequest.java
@@ -0,0 +1,41 @@
+package br.com.ifpe.look.api.produto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.ifpe.look.modelo.produto.Produto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoRequest {
+    
+    private String titulo;
+
+    private String codigo;
+ 
+    private String descricao;
+
+    private Double valorUnitario;
+    
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDeVencimento;
+
+    public Produto build() {
+
+        return Produto.builder()
+                .titulo(titulo)
+                .codigo(codigo)
+                .descricao(descricao)
+                .valorUnitario(valorUnitario)
+                .dataDeVencimento(dataDeVencimento)
+                .build();
+    }
+ 
+}
---
+++ src/main/java/br/com/ifpe/look/modelo/produto/ProdutoService.java
@@ -0,0 +1,40 @@
+package br.com.ifpe.look.modelo.produto;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ifpe.look.util.entity.GenericService;
+import br.com.ifpe.look.modelo.produto.Produto.ProdutoBuilder;
+
+
+@Service
+public class ProdutoService extends GenericService{
+    
+    @Autowired
+    private ProdutoRepository repository;
+ 
+    @Transactional
+    public Produto save(Produto produto) {
+ 
+        super.preencherCamposAuditoria(produto);
+        return repository.save(produto);
+    }
+
+    public Produto save(ProdutoBuilder build) {
+        return null;
+    }
+
+    public List<Produto> listarTodos() {
+  
+    return repository.findAll();
+}
+
+    public Produto obterPorID(Long id) {
+
+    return repository.findById(id).get();
+}
+}
