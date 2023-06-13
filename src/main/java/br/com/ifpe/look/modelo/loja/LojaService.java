72f258c1c0db1ff6e46772ea843a5cfa90f4bbc7
QuaiboC <crpas@discente.ifpe.edu.br>
2023-06-12 23:28:26 -0300

    T

--- src/main/java/br/com/ifpe/look/modelo/loja/LojaService.java
+++ src/main/java/br/com/ifpe/look/modelo/loja/LojaService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.ifpe.look.util.entity.GenericService;
 
+@Service
 public class LojaService extends GenericService{
     
     @Autowired
