fffc0c375460dea24a4b3e69f39d9c76530f49da
theo <theo@email.com>
2023-06-27 11:40:08 -0300

    Bug Loja resolvido - 2 Escopo

--- src/main/java/br/com/ifpe/look/api/loja/LojaRequest.java
+++ src/main/java/br/com/ifpe/look/api/loja/LojaRequest.java
@@ -4,7 +4,9 @@ import br.com.ifpe.look.modelo.loja.Loja;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.Data;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
